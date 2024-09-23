26a30564173766eeec498e1ca157743c133e5c34
justin-pyne <jpyne.justin@gmail.com>
2024-09-23 15:11:32 -0700

    generate message api call and  slashcmd handling

--- src/index.js
+++ src/index.js
@@ -17,10 +17,54 @@ const client = new Client({
     ]
 })
 
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+
 /** Test that bot is successfully online */
 client.on("ready", (c) => {
     console.log(`${c.user.tag} is ready.`);
 });
+
+/** Function for generating text response */
+async function generateMessage(textPrompt) {
+    const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+            {
+                role: 'system',
+                content: 'You are a student answering your friend\'s question.',
+            },
+            {
+                role: "user",
+                content: textPrompt,   
+            }
+        ]
+    });
+    return response.choices[0].message;
+}
+
+
+/** add functionality -- We handle slash commands */
+client.on("interactionCreate", async (interaction) => {
+    if (interaction.isChatInputCommand()) return;
+    if (interaction.channel.id !== process.env.CHANNEL_ID) return;
+
+    if (interaction.commandName === "ama"){
+        await interaction.deferReply();
+        const prompt = interaction.options.get("text").value();
+
+        try{
+            // Get response
+            response = await generateMessage(prompt);
+            await interaction.editReply(response);
+        } catch (e) {
+            console.log(`Error occurred: ${e}`);
+            return await interaction.editReply("IDK");
+        }
+    }
+})
 
 /** Start running bot instance */
 client.login(process.env.DISCORD_API_KEY);
