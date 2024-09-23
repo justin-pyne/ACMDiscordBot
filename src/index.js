e10df338b2b4f030775b1d5f64182ec3e29a9e73
justin-pyne <jpyne.justin@gmail.com>
2024-09-23 14:36:57 -0700

    testing bot online

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import dotenv from "dotenv";
 import { Client, IntentsBitField, EmbedBuilder } from "discord.js";
 
 /**
- * Initialize dotenv
+ * Initialize dotenv to access environment variables
  */
+dotenv.config();
 
+/** Set up client object */
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ]
+})
+
+/** Test that bot is successfully online */
+client.on("ready", (c) => {
+    console.log("${c.user.tag} is ready.");
+});
+
+/** Start running bot instance */
+client.login(process.env.DISCORD_API_KEY);
