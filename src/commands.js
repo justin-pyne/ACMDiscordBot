0de6c3c59e557b74bca1eed5ad07196279524951
justin-pyne <jpyne.justin@gmail.com>
2024-09-23 14:55:34 -0700

    commands js

--- src/commands.js
+++ src/commands.js
@@ -1,4 +1,39 @@
 import dotenv from "dotenv";
 import { REST, Routes, SlashCommandBuilder } from "discord.js";
 
+/** Define commands */
+const commands = [
+    new SlashCommandBuilder()
+        .setName("ama")
+        .setDescription("Ask me anything!")
+        .addStringOption(option => option.setName("text")
+                                        .setDescription("Body Text")
+                                        .setRequired(true)),
+    new SlashCommandBuilder()
+        .setName("genImg")
+        .setDescription("Generate an image given the prompt!")
+        .addStringOption(option => option.setName("text")
+                                        .setDescription("Body Text")
+                                        .setRequired(true)),
+]
 
+/** Register the commands */
+const rest = new REST().setToken(process.env.DISCORD_API_KEY);
+
+(async () => {
+    try{
+        console.log("Registering slash commands...");
+
+        await rest.put(
+            Routes.applicationGuildCommands(
+                process.env.CLIENT_ID,
+                process.env.SERVER_ID,
+            ),
+            { body : commands }
+        );
+
+        console.log("Commands registered successfully!");
+    } catch (e) {
+        console.log(`There was an error: ${e}`);
+    }
+})
